aa064394a5bf6cb16eac58ac04ad120237a9f1a9
Stefan Wienert <info@stefanwienert.de>
2022-04-27 17:50:44 +0200

    Reduce highlights

--- queries/html_tags/highlights.scm
+++ queries/html_tags/highlights.scm
@@ -2,8 +2,8 @@
 (erroneous_end_tag_name) @error
 (comment) @comment
 (attribute_name) @tag.attribute
-(attribute_value) @string
-(quoted_attribute_value) @string
+(attribute
+  (quoted_attribute_value) @string)
 (text) @none
 
 ((element (start_tag (tag_name) @_tag) (text) @text.title)
@@ -12,23 +12,8 @@
 ((element (start_tag (tag_name) @_tag) (text) @text.strong)
  (#match? @_tag "^(strong|b)$"))
 
-((element (start_tag (tag_name) @_tag) (text) @text.emphasis)
- (#match? @_tag "^(em|i)$"))
-
-((element (start_tag (tag_name) @_tag) (text) @text.strike)
- (#match? @_tag "^(s|del)$"))
-
-((element (start_tag (tag_name) @_tag) (text) @text.underline)
- (#eq? @_tag "u"))
-
 ((element (start_tag (tag_name) @_tag) (text) @text.literal)
  (#match? @_tag "^(code|kbd)$"))
-
-
-((attribute
-   (attribute_name) @_attr
-   (quoted_attribute_value (attribute_value) @text.uri))
- (#match? @_attr "^(href|src)$"))
 
 [
  "<"
